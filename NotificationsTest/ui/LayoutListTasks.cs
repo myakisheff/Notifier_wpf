7b10d5d8236ac5d392cfa8fdf048700e904883de
myakisheff <n.s.myakishev@yandex.ru>
2024-06-03 20:09:19 +0300

    added list of task layout

---
+++ NotificationsTest/ui/LayoutListTasks.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Notifier.ui
+{
+    public class LayoutListTasks : ListBox
+    {
+        public Layout layout
+        {
+            get { return (Layout)GetValue(layoutProperty); }
+            set { SetValue(layoutProperty, value); }
+        }
+
+        public static readonly DependencyProperty layoutProperty =
+            DependencyProperty.Register("layout", typeof(Layout), typeof(LayoutListTasks), new PropertyMetadata(Layout.List));
+    }
+
+    public enum Layout
+    {
+        Tile,
+        List
+    }
+}
